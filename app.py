1f2e1c06f2877b9a8cc8eac93a104a4e2487634f
Josh Morphew <josh.morphew@hotmail.com>
2023-09-09 17:33:52 +1000

    Progress on dashboard navbar

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
-
-# Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
