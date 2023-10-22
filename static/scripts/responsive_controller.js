c69d61cf00a3a95b9e5b7964908e01d78978d4d3
Josh Morphew <josh.morphew@hotmail.com>
2023-10-22 15:26:32 +1100

    fixed some responsive issues with nav collapse

--- static/scripts/responsive_controller.js
+++ static/scripts/responsive_controller.js
@@ -3,23 +3,36 @@ document.addEventListener('DOMContentLoaded', function() {
     const main = document.querySelector('main');
     const header = document.querySelector('header');
 
-    nav.addEventListener('click', function() {
-        if(window.innerWidth <= 870) {
-            nav.classList.add('expanded');
+    function collapseAll() {
+        const openElements = document.querySelectorAll('.collapse.show');
+        openElements.forEach(element => {
+            element.classList.remove('show');
+        });
+        // Remove active class from nav-item-title
+        const activeTitles = document.querySelectorAll('.nav-item-title.active');
+        activeTitles.forEach(title => {
+            title.classList.remove('active');
+        });
+    }
+
+    nav.addEventListener('click', function(e) {
+        e.stopPropagation();  // Prevent the click from propagating to the document
+
+        if (window.innerWidth <= 870) {
+            nav.classList.add('expanded');  // Add the expanded state
             main.classList.add('nav-expanded');
             header.classList.add('header-expanded');
 
-            // Set the overlay height based on the scrollable content
             main.style.setProperty('--overlay-height', `${main.scrollHeight}px`);
         }
     });
 
     document.addEventListener('click', function(e) {
-        if(!nav.contains(e.target) && nav.classList.contains('expanded')) {
+        if (!nav.contains(e.target)) {
             nav.classList.remove('expanded');
             main.classList.remove('nav-expanded');
             header.classList.remove('header-expanded');
+            collapseAll();
         }
     });
 });
-
