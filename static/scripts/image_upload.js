2c969e80864cdb06f3b20b4e551b722dff8b914e
Joshua Morphew <129724803+Joshnovski@users.noreply.github.com>
2023-10-26 22:03:49 +1100

    Update image_upload.js
    Reformatted code, changed fade out to wait 5 seconds not 3. Added more comments

--- static/scripts/image_upload.js
+++ static/scripts/image_upload.js
@@ -1,18 +1,24 @@
-// Trigger file input when upload button is clicked
+// Function to programmatically trigger the file input element when called.
 function triggerFileInput() 
 {
+    // Trigger a click event on the file input element with ID 'file'.
+    // This opens the file picker dialog for the user.
     document.getElementById('file').click();
 }
-
-// Hide flask message after 5 seconds
+// Wait until the document's content has fully loaded before executing the function.
 document.addEventListener('DOMContentLoaded', function() {
+    // Set a timer to execute after 5 seconds.
     setTimeout(function() {
-      var flashes = document.querySelectorAll('.flash');
-      flashes.forEach(function(flash) {
-        flash.style.opacity = 0;
-        flash.addEventListener('transitionend', function() {
-          flash.style.display = 'none';
+        // Get all elements with class 'flash' - typically used for temporary notification messages.
+        var flashes = document.querySelectorAll('.flash');
+        // Iterate through each flash message.
+        flashes.forEach(function(flash) {
+            // Gradually fade out the flash message by setting its opacity to 0.
+            flash.style.opacity = 0;
+            // When the CSS opacity transition ends, hide the flash message completely.
+            flash.addEventListener('transitionend', function() {
+                flash.style.display = 'none';
+            });
         });
-      });
-    }, 3000);
-  });
+    }, 5000);
+});
