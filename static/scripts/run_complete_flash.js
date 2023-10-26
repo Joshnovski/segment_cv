5564dcae230f8949a2010b87f259f97d0a98871c
Joshua Morphew <129724803+Joshnovski@users.noreply.github.com>
2023-10-26 22:12:24 +1100

    Update run_complete_flash.js
    Reformat and add comments. Can't be bothered doing the rest

--- static/scripts/run_complete_flash.js
+++ static/scripts/run_complete_flash.js
@@ -1,12 +1,17 @@
-// Hide flask message after 5 seconds
+// Ensure that the code runs after the document's content has fully loaded.
 document.addEventListener('DOMContentLoaded', function() {
+    // Set a timer to execute after 5 seconds (5000 milliseconds).
     setTimeout(function() {
-      var flashes = document.querySelectorAll('.flash');
-      flashes.forEach(function(flash) {
-        flash.style.opacity = 0;
-        flash.addEventListener('transitionend', function() {
-          flash.style.display = 'none';
+        // Select all elements with the class 'flash'.
+        var flashes = document.querySelectorAll('.flash');
+        // Iterate through each flash message.
+        flashes.forEach(function(flash) {
+            // Gradually fade out the flash message by setting its opacity to 0.
+            flash.style.opacity = 0;
+            // After the fade-out transition ends, the message will be hidden completely.
+            flash.addEventListener('transitionend', function() {
+                flash.style.display = 'none';
+            });
         });
-      });
-    }, 3000);
-  });
+    }, 5000);
+});
