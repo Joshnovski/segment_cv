5462ec71f420957dcdc8ba00e62a9ae16f227859
Joshua Morphew <129724803+Joshnovski@users.noreply.github.com>
2023-10-26 21:56:23 +1100

    Update click_dropdown.js
    Reformat and added more comments

--- static/scripts/click_dropdown.js
+++ static/scripts/click_dropdown.js
@@ -1,9 +1,12 @@
 function toggleBackgroundColor(event) {
+    // Get the element that triggered the event.
     var div = event.currentTarget;
-
+    // Check if the element has the 'active' class.
     if (div.classList.contains('active')) {
+        // If the element has the 'active' class, remove it.
         div.classList.remove('active');
     } else {
+        // Otherwise, add the 'active' class to the element.
         div.classList.add('active');
     }
 }
